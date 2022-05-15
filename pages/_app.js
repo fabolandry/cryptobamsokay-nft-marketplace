33a5d7576d122721fadf7f43238d917f83097e63
fabolandry <fabolandry@hotmail.com>
2022-05-14 18:34:32 -0700

    adding dope styling

--- pages/_app.js
+++ pages/_app.js
@@ -4,29 +4,41 @@ import Link from 'next/link'
 function MyApp({ Component, pageProps }) {
   return (
     <div>
-      <nav className="border-b p-2">
-        <p className="text-4xl font-bold">CryptoBamsOkay-NFT</p>
-        <div className="flex mt-4">
-          <Link href="/">
-            <a className="mr-4 text-pink-500">
-              Home
-            </a>
-          </Link>
-          <Link href="/create-nft">
-            <a className="mr-6 text-pink-500">
-              Sell NFT
-            </a>
-          </Link>
-          <Link href="/my-nfts">
-            <a className="mr-6 text-pink-500">
-              My NFTs
-            </a>
-          </Link>
-          <Link href="/dashboard">
-            <a className="mr-6 text-pink-500">
-              Dashboard
-            </a>
-          </Link>
+      <nav className="p-2 shadow-lg bg-fuchsia-200 border-bpy-4 navbar navbar-expand-lg navbar-light">
+        <div className="relative flex w-screen">
+          <div className="flex justify-center pt-1">
+            <img src="images/logo.png" alt="logo image"/>
+          </div>
+          <div className="flex justify-around flex-1 mr-auto">
+            <Link href="/">
+              <button class="relative px-10 py-2 group">
+                <span class="absolute inset-0 w-full h-full transition duration-300 ease-out transform translate-x-1 translate-y-1 bg-black group-hover:-translate-x-0 group-hover:-translate-y-0"></span>
+                <span class="absolute inset-0 w-full h-full bg-white border-2 border-black group-hover:bg-black"></span>
+                <span class="relative text-black group-hover:text-white"> Home </span>
+              </button>
+            </Link>
+            <Link href="/create-nft">
+              <button class="relative px-9 py-2 group">
+                <span class="absolute inset-0 w-full h-full transition duration-300 ease-out transform translate-x-1 translate-y-1 bg-red-700 group-hover:-translate-x-0 group-hover:-translate-y-0"></span>
+                <span class="absolute inset-0 w-full h-full bg-white border-2 border-red-700 group-hover:bg-red-700"></span>
+                <span class="relative text-red-700 group-hover:text-red-100"> Sell NFT </span>
+              </button>
+            </Link>
+            <Link href="/my-nfts">
+              <button class="relative px-9 py-2 group">
+                <span class="absolute inset-0 w-full h-full transition duration-300 ease-out transform translate-x-1 translate-y-1 bg-cyan-700 group-hover:-translate-x-0 group-hover:-translate-y-0"></span>
+                <span class="absolute inset-0 w-full h-full bg-white border-2 border-cyan-700 group-hover:bg-cyan-700"></span>
+                <span class="relative text-cyan-700 group-hover:text-cyan-100">My NFT</span>
+              </button>
+            </Link>
+            <Link href="/dashboard">
+              <button class="relative px-6 py-2 group">
+                <span class="absolute inset-0 w-full h-full transition duration-200 ease-out transform translate-x-1 translate-y-1 bg-indigo-700 group-hover:-translate-x-0 group-hover:-translate-y-0"></span>
+                <span class="absolute inset-0 w-full h-full bg-white border-2 border-indigo-700 group-hover:bg-indigo-700"></span>
+                <span class="relative text-indigo-700 group-hover:text-indigo-100"> Dashboard </span>
+              </button>
+            </Link>
+          </div>
         </div>
       </nav>
       <Component {...pageProps} />
--- pages/index.js
+++ pages/index.js
@@ -59,14 +59,14 @@ export default function Home() {
     await transaction.wait()
     loadNFTs()
   }
-  if (loadingState === 'loaded' && !nfts.length) return (<h1 className="px-20 py-10 text-3xl">No items in marketplace</h1>)
+  if (loadingState === 'loaded' && !nfts.length) return (<h1 className="px-20 py-10 text-5xl text-center text-purple-300 font-RubikGlitch">No items in marketplace</h1>)
   return (
     <div className="flex justify-center">
       <div className="px-4" style={{ maxWidth: '1600px' }}>
-        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-4 pt-4">
+        <div className="grid grid-cols-1 gap-4 pt-4 sm:grid-cols-2 lg:grid-cols-4">
           {
             nfts.map((nft, i) => (
-              <div key={i} className="border shadow rounded-xl overflow-hidden">
+              <div key={i} className="overflow-hidden border shadow rounded-xl">
                 <img src={nft.image} />
                 <div className="p-4">
                   <p style={{ height: '64px' }} className="text-2xl font-semibold">{nft.name}</p>
@@ -76,7 +76,7 @@ export default function Home() {
                 </div>
                 <div className="p-4 bg-black">
                   <p className="text-2xl font-bold text-white">{nft.price} ETH</p>
-                  <button className="mt-4 w-full bg-pink-500 text-white font-bold py-2 px-12 rounded" onClick={() => buyNft(nft)}>Buy</button>
+                  <button className="w-full px-12 py-2 mt-4 font-bold text-white bg-pink-500 rounded" onClick={() => buyNft(nft)}>Buy</button>
                 </div>
               </div>
             ))
